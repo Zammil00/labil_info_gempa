33c3e97c913dc8432895f769546044badde709d6
INI_LABIL <115417258+Zammil00@users.noreply.github.com>
2025-02-19 04:14:28 +0700

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_file.dart';
+import 'package:labil_info_gempa/page/page_home.dart';
 import 'package:labil_info_gempa/page/splash_page.dart';
+import 'package:intl/intl.dart';
 
 void run() {
-  runApp(LabilApp());
+  Intl.defaultLocale = 'in';
+  initializeDateFormatting('in', 'null');
+  runApp(MyApp());
 }
 
-class LabilApp extends StatelessWidget {
-  const LabilApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashPage(),
+      debugShowCheckedModeBanner: false,
+      title: 'INFO GEMPA TERKINI',
+      home: HomePage(),
     );
   }
 }
